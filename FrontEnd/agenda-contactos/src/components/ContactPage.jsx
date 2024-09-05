8c99de1b25e61c33bac3e1daa681349bbcda2954
NozoDev <kevinroj326@gmail.com>
2024-09-05 10:58:13 -0500

    uy login hecho, tambien realizando la pag de contact

--- FrontEnd/agenda-contactos/src/components/ContactPage.jsx
+++ FrontEnd/agenda-contactos/src/components/ContactPage.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const ContactPage = () => {
-  
+export const ContactPage = ({ onLogout }) => {
+  const [contactos, setContactos] = useState([
+    {
+      nombre: "John Doe",
+      telefono: "1234567890",
+      email: "johndoe@example.com",
+    },
+    {
+      nombre: "Jane Smith",
+      telefono: "9876543210",
+      email: "janesmith@example.com",
+    },
+  ]);
   return (
-    <div>
-    
+    <div className="container mx-auto p-4">
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-3xl font-bold text-gray-800">Mis contactos </h1>
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          onClick={onLogout}
+        >
+          Cerrar Sesión
+        </button>
+      </div>
+
+      {/* Formularioa para añadir/editar contactos */}
+
+      <form onSubmit={editingContact ? updateContact : addContact} className="mb-8"></form>
     </div>
   );
 };
--- FrontEnd/agenda-contactos/src/components/LandingPage.jsx
+++ FrontEnd/agenda-contactos/src/components/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export const LandingPage = (onLogin) => {
+export const LandingPage = ({ onLogin }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -9,37 +9,54 @@ export const LandingPage = (onLogin) => {
     onLogin(email, password);
   };
   return (
-    <div>
-      <div>
-        <div>
-          <h1>Agenda de contactos</h1>
-        </div>
-        <div>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="email">
-                <input
-                  type="email"
-                  id="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </label>
-            </div>
-
-            <div>
-              <label htmlFor="password">
-                <input
-                  type="password"
-                  id="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </label>
-            </div>
-          </form>
-        </div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
+          <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
+            Agenda de contactos
+          </h2>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              className="shadow hover:border-blue-300  appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              placeholder="Tu email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Contraseña
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+              Iniciar sesión
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
