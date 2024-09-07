1c1e8110362dcb4edd0bd029e1eeb855b946a860
Juan Bedoya <juanchischis2010@hotmail.com>
2024-09-07 13:00:19 -0500

    documentacionswagger

--- BackEnd/Contacts/settings.py
+++ BackEnd/Contacts/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'mycontacts',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- BackEnd/mycontacts/urls.py
+++ BackEnd/mycontacts/urls.py
@@ -1,5 +1,18 @@
 from django.urls import path
 from .views import Register, Login, ContactList, CreateContact, Logout, UpdateContact, DeleteContact, SearchContact, ExportContacts
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="API documentation for MyContacts",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('register/', Register.as_view(), name='Register'),
@@ -11,6 +24,7 @@ urlpatterns = [
     path('delete-contact/', DeleteContact.as_view(), name='DeleteContact'),
     path('search-contact/', SearchContact.as_view(), name='SearchContact'),
     path('export-contacts/', ExportContacts.as_view(), name='exportContacts'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
     
 
