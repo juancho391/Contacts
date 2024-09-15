5d5632fcde5cecf470397e8c6942d83b2aa6abaa
Juan Bedoya <juanchischis2010@hotmail.com>
2024-09-15 11:37:28 -0500

    configsshbuild

--- BackEnd/build.sh
+++ BackEnd/build.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+
 set -o errexit
 
+echo "Changing directory to BackEnd..."
 cd BackEnd
 
+echo "Installing dependencies..."
 pip install -r requirements.txt
 
+echo "Collecting static files..."
 python manage.py collectstatic --no-input
+
+echo "Applying migrations..."
 python manage.py migrate
+
+echo "Build and deployment script completed successfully."
